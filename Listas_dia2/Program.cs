a8347ac19e25c19596e55fb37c28b43e5cb83f27
edgardoeg <edgardoeg@yahoo.com>
2025-02-05 18:56:59 -0600

    Add project files.

---
+++ Listas_dia2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+List<string> listaString = new List<string>();
+listaString.Add("Naranja");
+listaString.Add("5");
+listaString.Add("Real Espana");
+listaString.Add("Barcelona");
+listaString.Add("Real Madrid");
+listaString.Add("Alajuela");
+listaString.Insert(0, "Real Madrid");
+//listaString.Remove("Real Madrid");//
+//listaString.RemoveAt(10);// Tener cuidado de no agregar un indice fuera del rango
+//listaString.RemoveAll(lista => lista.Contains("Real Madrid"));//
+
+foreach (string item in listaString)
+{
+    //Console.WriteLine($"{listaString.IndexOf(item)}: {item}");//
+    Console.WriteLine($"Item: {item}");
+}
